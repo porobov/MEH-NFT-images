37ecaad274ae5f17cfd901aee5e20770ca0fc66f
Petr Porobov <porobov@users.noreply.github.com>
2024-03-26 12:00:01 +0400

    MEH init

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const path = require('path')
-const moment = require('moment')
 const { HOST } = require('./src/constants')
-const db = require('./src/database')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 const app = express()
   .set('port', PORT)
@@ -14,24 +12,20 @@ const app = express()
 // Static public files
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res) {
-  res.send('Get ready for OpenSea!');
-})
+// app.get('/', function(req, res) {
+//   res.send('Get ready for OpenSea!');
+// })
 
 app.get('/api/token/:token_id', function(req, res) {
   const tokenId = parseInt(req.params.token_id).toString()
-  const person = db[tokenId]
-  const bdayParts = person.birthday.split(' ')
-  const day = parseInt(bdayParts[1])
-  const month = parseInt(bdayParts[0])
   const data = {
-    'name': person.name,
+    'name': 'Tile #' + tokenId,
     'attributes': {
-      'birthday': person.birthday,
-      'birth month': monthName(month),
-      'zodiac sign': zodiac(day, month),
-      // 'age': moment().diff(person.birthday, 'years')
+      'X Coordinate': '2',
+      'Y Coordinate': '2',
     },
+    'description': 'Ownership of tile #' + tokenId + ' at https://themillionetherhomepage.com. Owner can put ads within the area they own.',
+    'external_url': "",
     'image': `${HOST}/images/${tokenId}.png`
   }
   res.send(data)
@@ -40,16 +34,3 @@ app.get('/api/token/:token_id', function(req, res) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
-
-// returns the zodiac sign according to day and month ( https://coursesweb.net/javascript/zodiac-signs_cs )
-function zodiac(day, month) {
-  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
-  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
-}
-
-function monthName(month) {
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ]
-  return monthNames[month - 1]
-}
